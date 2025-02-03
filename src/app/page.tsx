40d31dc71dc1c8387870b567ec0095f0a10e2dc7
zafkiel45 <zafkielc@gmail.com>
2025-02-03 10:40:02 -0300

    feat(mobile): about me section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           />
         </Nav>
       </Header>
-      <Main>
+      <Main className="flex flex-col gap-5">
         <div className="flex justify-center">
           <div className="w-3/5 flex flex-col gap-2">
             <h1 className="text-2xl">
@@ -45,6 +45,30 @@ export default function Home() {
           <ContainerSVG className="w-2/5 h-full">
             <MainSVG className="w-full h-full"/>
           </ContainerSVG>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="w-full">
+            <h1 className="text-2xl font-semibold">
+              Sobre mim:
+            </h1>
+          </div>
+          <div>
+            <p>
+              Opa! Eu me chamo Murilo e eu sou atualmente um 
+              <StandOutText> desenvolvedor Front-End.</StandOutText>
+              <br />
+              <br />
+              Meu objetivo atual é <StandOutText>fortalecer os fundamentos</StandOutText>, 
+              aprimorar pontos que <StandOutText>precisam de refinamento</StandOutText>, 
+              e ir escalando pouco a pouco <StandOutText>rumo ao meu objetivo!</StandOutText>
+              <br />
+              <br />
+              A longo prazo eu almejo me tornar 
+              um <StandOutText>desenvolvedor Full-Stack</StandOutText>, 
+              cujo domina ambos os mundos: Front-End e Back-End.
+            </p>
+          </div>
+          
         </div>
       </Main>
     </>
