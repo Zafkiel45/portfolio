d0cb0e61a0613ef1b664702d57d63719956386e9
zafkiel45 <zafkielc@gmail.com>
2025-05-22 09:18:08 -0300

    refatore(roadmap): improved responsivity on mobiles

--- src/app/roadmap/[skill]/page.tsx
+++ src/app/roadmap/[skill]/page.tsx
@@ -6,18 +6,18 @@ import Image from "next/image";
 import { DoneSvg } from "../../../../public/svg/done";
 import { PendentSvg } from "../../../../public/svg/pendent";
 import { JavaScriptSvg } from "@/components/svg/javascript";
+import Link from "next/link";
+import { MainScreenButton } from "@/components/main_screen/button_main_screen.component";
 
 type Roadmap = {
   name: string;
   icon: React.JSX.Element;
-  goals: 
-    {
-      name: string;
-      isFinished: boolean;
-      startedAt: string;
-      finishedAt: string;
-    }[]
-  ;
+  goals: {
+    name: string;
+    isFinished: boolean;
+    startedAt: string;
+    finishedAt: string;
+  }[];
 };
 
 export default async function RoadmapRoute({
@@ -112,119 +112,119 @@ export default async function RoadmapRoute({
       ],
     },
     {
-      name: 'javascript',
-      icon: <JavaScriptSvg className="sm:h-11 sm:w-11 h-9 w-9"/>,
+      name: "javascript",
+      icon: <JavaScriptSvg className="sm:h-11 sm:w-11 h-9 w-9" />,
       goals: [
         {
           name: "Javascript Variables",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Datatypes",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Prototypes",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Type Casting",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Indexed collections",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Keyed Collections",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Equality Comparisons",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Loops and Iterations",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Control Flow",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Functions",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "DOM APIs",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Strict Mode",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "'This' Keyword",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Asynchronous JavaScript",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Event Loop",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Working with APIs",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Modules",
           isFinished: true,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
         {
           name: "Javascript Iterators and Generators",
           isFinished: false,
-          finishedAt: '?',
-          startedAt: '?'
+          finishedAt: "?",
+          startedAt: "?",
         },
-      ]
-    }
+      ],
+    },
   ];
 
   const { skill } = await params;
@@ -241,10 +241,18 @@ export default async function RoadmapRoute({
       <div className="flex h-screen items-center">
         {/* Main Text Container */}
         <div className="flex h-full relative 2xl:gap-5 gap-5 justify-center bottom-5 flex-col sm:gap-6">
-          <StandOutTitle isCentralized={false} title="Roadmap" />
-          <StandOutDescription isCentralized={false}>
-            Acompanhe minha jornada nesse mar de aventuras chamado: Programação.
-          </StandOutDescription>
+          <div className="flex flex-col gap-4">
+            <StandOutTitle isCentralized={false} title="Roadmap" />
+            <StandOutDescription isCentralized={false}>
+              Acompanhe minha jornada nesse mar de aventuras chamado:
+              Programação.
+            </StandOutDescription>
+          </div>
+          <div>
+            <Link href={'/'}>
+              <MainScreenButton btnContent="Página inicial" />
+            </Link>
+          </div>
         </div>
         {/* Svg of main container */}
         <div className="hidden sm:block">
@@ -257,36 +265,33 @@ export default async function RoadmapRoute({
       <div className="w-full h-fit">
         <div>
           <div className="bg-[#0c0c0c] shadow-md p-5 flex gap-2 items-center rounded-t-md border border-[#444444]">
-            <div>
-              {currentRoadmap[0].icon}
-            </div>
-            <div className="capitalize">
-              {currentRoadmap[0].name}
-            </div>
+            <div>{currentRoadmap[0].icon}</div>
+            <div className="capitalize">{currentRoadmap[0].name}</div>
           </div>
           {/* List of Goals of current Map*/}
           {currentRoadmap[0].goals.map((item, idx) => {
             return (
-              <div key={idx + 1} className="flex last:rounded-b-md p-5 border border-[#444444] justify-between w-full">
+              <div
+                key={idx + 1}
+                className="flex last:rounded-b-md p-5 border border-[#444444] justify-between w-full"
+              >
                 {/* name and Icon */}
                 <div className="flex gap-2 items-center w-full">
                   <div>
                     {item.isFinished ? (
-                      <DoneSvg className="sm:h-9 sm:w-9 h-6 w-6 fill-green-400"/>
+                      <DoneSvg className="sm:h-9 sm:w-9 h-6 w-6 fill-green-400" />
                     ) : (
-                      <PendentSvg className="sm:h-9 sm:w-9 h-6 w-6 fill-red-400"/>
+                      <PendentSvg className="sm:h-9 sm:w-9 h-6 w-6 fill-red-400" />
                     )}
                   </div>
                   <div className="sm:text-lg">{item.name}</div>
                 </div>
                 {/* Dates */}
-                <div className="flex gap-2 text-slate-400 items-center">
-                  from 
+                <div className="hidden sm:flex gap-2 text-slate-400 items-center">
+                  from
                   <div>{item.startedAt}</div>
                   to
-                  <div>
-                    {item.finishedAt}
-                  </div>
+                  <div>{item.finishedAt}</div>
                 </div>
               </div>
             );
--- src/components/nodes/standout_description.tsx
+++ src/components/nodes/standout_description.tsx
@@ -2,7 +2,7 @@ import type { StandoutDescrition } from "@/types/standout_texts";
 
 export function StandOutDescription({children, isCentralized}: StandoutDescrition) {
   return (
-    <div className={`${isCentralized ? 'md:text-center':''} text-balance sm:text-lg`}>
+    <div className={`${isCentralized ? 'md:text-center':''} text-balance min-[420px]:text-pretty sm:text-lg`}>
       <p>
         {children}
       </p>
